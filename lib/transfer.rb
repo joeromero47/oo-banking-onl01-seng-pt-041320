f27a45ec0875ac298188b0149616e724a1d4bb64
Joseph Romero <joeromero47@gmail.com>
2020-05-15 17:51:31 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
   end 
   
   def execute_transaction 
-    if @sender.balance > @amount && BankAccount.valid?
+    if @sender.balance > @amount && @sender.valid?
       @sender.balance -= @amount 
       @receiver.balance += @amount 
       @status = "complete"
