b30353cc63488d90be6ba15d7a6119273bef5bc0
Joseph Romero <joeromero47@gmail.com>
2020-05-15 17:50:12 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
   end 
   
   def execute_transaction 
-    if @sender.balance > @amount && @status == "pending"
+    if @sender.balance > @amount && @status.valid?
       @sender.balance -= @amount 
       @receiver.balance += @amount 
       @status = "complete"
